fcb4d0ad06b1950f07004d19d53f9d5ae0a1b359
Yushuf Sharker <yushuf@i21a-s1.ufhpc>
2016-09-13 12:00:07 -0400

    Second version

--- weather.sh
+++ weather.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-x=$1
+x=`curl "https://www.wunderground.com/history/airport/GNV/2016/09/06/DailyHistory.heml?&format=1"| cat |grep -E "\w+M\,"|awk '{print $2}'| awk -F',' '{print $2}'|sort -nr| head -n1 | awk '{$0=int($0)}1'`
 echo The value of x is $x
 if [ $x -le 50 ]
 then
