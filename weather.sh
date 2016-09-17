1f58b66b247b4bb7041a94e868ca71b61c82879e
Yushuf Sharker <yushuf@i21a-s1.ufhpc>
2016-09-17 12:01:34 -0400

    Option to get yesterday or any day weather corrected for leap year

--- weather.sh
+++ weather.sh
@@ -1,21 +1,48 @@
 #!/bin/bash
-echo "Day and press Enter"
-read day
-echo "Month and press Enter"
-read month
-echo "Year in 4 digit and press Enter"
-read year
-x=`curl "https://www.wunderground.com/history/airport/GNV/$year/$month/$day/DailyHistory.heml?&format=1"| \
+echo "Which day you are interested in 1: Yesterday, 2: any previous day"
+read dt
+
+if [ $dt -eq 1 ]
+then
+	day=`date -d yesterday +%d`
+	month=`date -d yesterday +%m`
+	year=`date -d yesterday +%Y`	 
+else
+	echo "Enter your day month and year"
+	echo "Day and press Enter"
+	read day
+	echo "Month and press Enter"
+	read month
+	echo "Year in 4 digit and press Enter"
+	read year
+
+	year2=$(( $year % 4 ))
+
+        if [ $year2 -ne 0 ] && [ $day -eq 29 ] && [ $month -eq 2 ]
+	then
+	    echo "$y is not a Leap Year!, the date does not exist"
+	    echo "run the process again"
+	    exit 1
+		
+	fi
+
+fi
+
+x=`curl -s "https://www.wunderground.com/history/airport/GNV/$year/$month/$day/DailyHistory.heml?&format=1"| \
 	cat |grep -E "\w+M\,"|awk '{print $2}'| awk -F',' '{print $2}'|sort -nr| head -n1 | \
 	awk '{$0=int($0)}1'`
-echo The value of x is $x
+
+echo Temperature on that day was round to $x degrees Fahrenheit
+
 if [ $x -le 50 ]
 then
-   echo "It is cold"
+   echo "It was cold"
+
 elif [ $x -ge 51 ] && [ $x -le 80 ]
 then
    echo "Ah, nice day"
+
 else
-   echo "Too hot!"
+   echo "It was too hot!"
 fi
 
